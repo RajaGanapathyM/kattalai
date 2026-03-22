7ed78dc5bc782fdc4bce0e64bb51da4c71f4e634
RG <rganapathy6@gmail.com>
2026-03-22 21:22:33 +0530

    torch declared

--- pysrc/soulengine/__init__.py
+++ pysrc/soulengine/__init__.py
@@ -6,12 +6,15 @@ from datetime import datetime
 from typing import Optional
 import logging
 import json
+from importlib import reload
 try:
+    
+    import torch
     _torch_lib = os.path.join(os.path.dirname(torch.__file__), "lib")
     print("Torch Lib:",_torch_lib)
     if os.path.exists(_torch_lib):
         os.add_dll_directory(_torch_lib)
-    import torch
+    reload(torch)
     
 except Exception  as e:
     print(str(e))
